3518e1287697628c97cf10b3458b2deacbf1b127
Robin Ince <robince@gmail.com>
2024-10-16 13:34:07 +0100

    found the logger and config submodule a bit annoying

--- scripts/ri-json-from-html.py
+++ scripts/ri-json-from-html.py
@@ -12,3 +12,13 @@ lesson_data = json.loads(script_tag.string)
 with open(DATA_DIR / "sample_lesson_next_data.json", "w") as file:
     json.dump(lesson_data, file, indent=2)
 # %%
+
+# Download all 
+# <a role="link" data-testid="download-all-button" title="Download all resources"
+#     href="/teachers/programmes/biology-secondary-ks4-foundation-aqa/units/eukaryotic-and-prokaryotic-cells/lessons/cells/downloads?preselected=all"
+#     class="sc-5368bac2-0 gJhAja">
+
+# Share all
+# <a role="link" data-testid="share-all-button" title="Share activities with pupils" aria-disabled="false"
+#     href="/teachers/programmes/biology-secondary-ks4-foundation-aqa/units/eukaryotic-and-prokaryotic-cells/lessons/cells/share?preselected=all"
+#     class="sc-5368bac2-0 gJhAja">
