b9c64a2e6c3235787bd67c59a17c0aded691620e
Robin Ince <robince@gmail.com>
2024-10-16 13:17:38 +0100

    test dvc move

--- scripts/ri-json-from-html.py
+++ scripts/ri-json-from-html.py
@@ -2,6 +2,7 @@
 from oakhack.config import PROJ_ROOT, DATA_DIR
 from bs4 import BeautifulSoup
 import json
+DATA_DIR = DATA_DIR / "sample_lesson"
 
 with open(DATA_DIR / "sample_lesson.html", "r") as file:
     soup = BeautifulSoup(file, "lxml")
