59564115b1f07d57cb8f57d78c20e1966d34737a
Robin Ince <robince@gmail.com>
2024-10-16 13:08:30 +0100

    extract __NEXT_DATA__ json from lesson html page

---
+++ scripts/ri-json-from-html.py
@@ -0,0 +1,13 @@
+# %%
+from oakhack.config import PROJ_ROOT, DATA_DIR
+from bs4 import BeautifulSoup
+import json
+
+with open(DATA_DIR / "sample_lesson.html", "r") as file:
+    soup = BeautifulSoup(file, "lxml")
+# %%
+script_tag = soup.find("script", {"id": "__NEXT_DATA__"})
+lesson_data = json.loads(script_tag.string)
+with open(DATA_DIR / "sample_lesson_next_data.json", "w") as file:
+    json.dump(lesson_data, file, indent=2)
+# %%
