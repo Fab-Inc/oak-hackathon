2ef10765f92ee0119d2e9a48f6af8cc229f04617
Oliver Garrod <oliver.garrod@glasgow.ac.uk>
2024-10-18 12:27:25 +0100

    Refactored the code from ri-load-json-data.py into oakhack.utils.load_oak_lessons and oakhack.utils.load_oak_lessons_df

---
+++ oakhack/utils.py
@@ -0,0 +1,98 @@
+import zipfile as zf
+import json
+import itertools as it
+
+import pandas as pd
+
+from . import DATA_DIR
+
+
+def get_programmes_by_ks(programmes, ks):
+    return {k: v for k, v in programmes.items() if v["keyStageSlug"] == f"ks{ks}"}
+
+
+def get_programmes_by_subject(programmes, subject):
+    return {k: v for k, v in programmes.items() if v["subjectSlug"] == subject}
+
+
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+
+def load_oak_lessons(oak_json_file=DATA_DIR / "oak_json.zip"):
+    with zf.ZipFile(oak_json_file) as zip:
+        
+        # load programmes
+        info = zip.infolist()
+        programme_filenames = [
+            f.filename for f in info if f.is_dir() and len(f.filename.split("/")) == 3
+        ]
+        programmes = {}
+        for p in programme_filenames:
+            with zip.open(p + "units.json") as f:
+                pj = json.load(f)["props"]["pageProps"]["curriculumData"]
+            programmes[pj["programmeSlug"]] = pj
+
+        # load units
+        units_by_programme = {}
+        for pslug, p in programmes.items():
+            units_by_programme[pslug] = {}
+            for unit in p["units"]:
+                if unit[0]["cohort"] != "2023-2024":
+                    continue
+
+                fname = "oak_scraped_json/" + pslug + "/" + unit[0]["slug"] + "/lessons.json"
+                with zip.open(fname) as f:
+                    units_by_programme[pslug][unit[0]["slug"]] = json.load(f)["props"][
+                        "pageProps"
+                    ]["curriculumData"]
+
+        # load lessons
+        lessons_by_unit = {}
+        for units in units_by_programme.values():
+            for unit in units.values():
+                unit_path = unit["programmeSlug"] + "/" + unit["unitSlug"]
+                base_path = "oak_scraped_json/" + unit_path
+                lessons_by_unit[unit_path] = {}
+
+                for lesson in unit["lessons"]:
+                    fname = base_path + "/" + lesson["lessonSlug"] + ".json"
+                    with zip.open(fname) as f:
+                        lessons_by_unit[unit_path][lesson["lessonSlug"]] = json.load(f)["props"][
+                            "pageProps"
+                        ]["curriculumData"]
+
+    return flatten([[l for l in us.values()] for us in lessons_by_unit.values()])
+
+
+def load_oak_lessons_df(oak_json_file=DATA_DIR / "oak_json.zip"):
+    lessons_l = load_oak_lessons(oak_json_file=oak_json_file)
+    # lessons_df
+    column_keys = [
+        "lessonSlug",
+        "lessonTitle",
+        "programmeSlug",
+        "unitSlug",
+        "keyStageSlug",
+        "tierSlug",
+        "contentGuidance",
+        "additionalMaterialUrl",
+        "worksheetUrl",
+        "presentationUrl",
+        # "transcriptSentences",
+        "videoTitle",
+        "subjectSlug",
+        # "subjectTitle",
+        # "upplementary-pdf",
+        # "supplementary-docx",
+    ]
+
+    df_data = {}
+    for col in column_keys:
+        df_data[col] = [l[col] for l in lessons_l]
+
+    df_data["json"] = [json.dumps(l) for l in lessons_l]
+    df_data["unitKey"] = [l["programmeSlug"] + "/" + l["unitSlug"] for l in lessons_l]
+    lessons_df = pd.DataFrame(df_data)
+
+    return lessons_df
--- scripts/og-json-fom-zip.py
+++ scripts/og-json-fom-zip.py
@@ -1,4 +1,5 @@
 # %%
+from typing import Optional, Literal
 import json
 import fnmatch
 from zipfile import ZipFile
@@ -36,3 +37,95 @@ with ZipFile(jsonzipfile, "r") as zip:
     for name in namelist_filtered:
         with zip.open(name) as f:
             jsonlist_filtered.append(json.load(f))
+
+# %%
+with ZipFile(jsonzipfile, "r") as zip:
+    namelist = zip.namelist()
+
+    namelist_filtered = fnmatch.filter(namelist, "*/units.json")
+    # load all units
+    units_json = []
+    for name in namelist_filtered:
+        with zip.open(name) as f:
+            units_json.append(json.load(f)["props"]["pageProps"]["curriculumData"])
+
+# %%
+units_json[0]
+
+
+# %%
+class OakLessonJson:
+    def __init__(self, jsonlist: Optional[list] = None):
+        if jsonlist is None:
+            jsonlist = []
+        self.jsonlist = jsonlist
+        self._gen_query_keys_vals()
+
+    def append(self, val):
+        self.jsonlist.append(val)
+        self._gen_query_keys_vals()
+
+    def _gen_query_keys_vals(self):
+        self.query_keys = set(
+            [
+                key
+                for ujson in self.jsonlist
+                for key, val in ujson.items()
+                if not isinstance(val, (list, dict))
+            ]
+        )
+        self.query_key_vals = {
+            key: set([ujson[key] for ujson in self.jsonlist]) for key in self.query_keys
+        }
+
+    def query(self, **query):
+        outjslist = []
+        for json in self.jsonlist:
+            for key, val in query.items():
+                if json[key] == val:
+                    outjslist.append(json)
+                    break
+        return outjslist
+
+
+class OakLevel:
+    def __init__(
+        self, level: Literal["programme", "unit"], parent: str="", catfile=DATA_DIR / "oak_json.zip"
+    ):
+        self.catfile = catfile
+        if level == "programme":
+            jsfile = "units.json"
+        elif level == "unit":
+            jsfile = "lessons.json"
+        with ZipFile(catfile, "r") as zip:
+            namelist = zip.namelist()
+            namelist_filtered = fnmatch.filter(namelist, f"*{parent}/{jsfile}")
+            # load all units
+            self.jsonlist = OakLessonJson()
+            for name in namelist_filtered:
+                with zip.open(name) as f:
+                    self.jsonlist.append(json.load(f)["props"]["pageProps"]["curriculumData"])
+
+    def query(self, **query):
+        return [
+            OakLevel(self.jsonlist.query(**query))
+        ]
+
+    @property
+    def query_keys(self):
+        return self.jsonlist.query_keys
+
+    @property
+    def query_key_vals(self):
+        return self.jsonlist.query_key_vals
+
+
+# %%
+oaklprog = OakLevel("programme")
+
+print(oaklprog.query_keys)
+print(oaklprog.query_key_vals)
+
+query_res = oaklprog.query(subjectTitle="Biology", phase="secondary", keyStageSlug="ks4")
+print(len(query_res))
+print(query_res[0])
