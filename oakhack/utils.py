cf79f53fd9710f52a2560e4d63e2e37f4d79adfd
Robin Ince <robince@gmail.com>
2024-10-20 17:21:08 +0100

    work around circular import of DATA_DIR

--- oakhack/utils.py
+++ oakhack/utils.py
@@ -1,11 +1,21 @@
 import zipfile as zf
 import json
 import itertools as it
-
+from pathlib import Path
 import pandas as pd
 
-from . import DATA_DIR
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+# logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
+DATA_DIR = PROJ_ROOT / "data"
+# RAW_DATA_DIR = DATA_DIR / "raw"
+# PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+# MODELS_DIR = PROJ_ROOT / "models"
+
+# REPORTS_DIR = PROJ_ROOT / "reports"
+# FIGURES_DIR = REPORTS_DIR / "figures"
 
 def get_programmes_by_ks(programmes, ks):
     return {k: v for k, v in programmes.items() if v["keyStageSlug"] == f"ks{ks}"}
