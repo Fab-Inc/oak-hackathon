aaf9bf878ae64130d76b6f535fd3972573e42542
Robin Ince <robince@gmail.com>
2024-10-21 11:02:29 +0100

    extract question text

--- scripts/ri-explore-questions.py
+++ scripts/ri-explore-questions.py
@@ -13,8 +13,17 @@ lessons, l_df = oh.utils.load_oak_lessons_with_df()
 questions, q_df = oh.utils.extract_questions(lessons)
 
 # %%
+question_types = q_df.questionType.unique()
+# ['multiple-choice', 'order', 'match', 'short-answer', 'explanatory-text']
+qs_by_type = {
+    k: [questions[i] for i in q_df.query(f"questionType == '{k}'").index] for k in question_types
+}
+# order_qs = [questions[i] for i in q_df.query("questionType == 'order'").index]
 
-question = questions[0]
+# %%
+
+# question = questions[0]
+question = qs_by_type["explanatory-text"][0]
 q_text = []
 q_image = []
 for block in question["questionStem"]:
@@ -22,6 +31,7 @@ for block in question["questionStem"]:
         q_text.append(block["text"])
     if block["type"] == "image":
         q_image.append(block["imageObject"]["url"])
+
 if question["questionType"] == "multiple-choice":
     for answer in question["answers"]["multiple-choice"]:
         # only keep correct answer options
@@ -31,6 +41,35 @@ if question["questionType"] == "multiple-choice":
                     q_text.append(block["text"])
                 else:
                     print(f"found multiple-choice answer block of type: {block["type"]}")
+
+elif question["questionType"] == "order":
+    for answer in question["answers"]["order"]:
+        # add all answer options (ignore order)
+        for block in answer["answer"]:
+            if block["type"] == "text":
+                q_text.append(block["text"])
+            else:
+                print(f"found order answer block of type: {block["type"]}")
+
+elif question["questionType"] == "match":
+    for answer in question["answers"]["match"]:
+        # add all answer stems and match words
+        for blocks in it.chain([answer[k] for k in ["correctChoice", "matchOption"]]):
+            for block in blocks:
+                if block["type"] == "text":
+                    q_text.append(block["text"])
+                else:
+                    print(f"found match answer block of type: {block["type"]}")
+
+elif question["questionType"] == "short-answer":
+    for answer in question["answers"]["short-answer"]:
+        for block in answer["answer"]:
+            if block["type"] == "text":
+                q_text.append(block["text"])
+            else:
+                print(f"found short-answer answer block of type: {block["type"]}")
+
 q_text.extend([question[k] for k in ["hint", "feedback"]])
+
 
 # %%
