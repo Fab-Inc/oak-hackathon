01aa2b8227424aec03a5f4114a75249dd8a88dfb
Oliver Garrod <oliver.garrod@glasgow.ac.uk>
2024-10-22 10:31:02 +0100

    optimised the calculation of earlier, and removed later from calc

--- scripts/ri-build-klp-graph.py
+++ scripts/ri-build-klp-graph.py
@@ -11,44 +11,39 @@ import oakhack as oh
 # %%
 lessons, l_df = oh.utils.load_oak_lessons_with_df()
 flat_klp = oh.utils.extract_klp(lessons)
+
 # %%
 klpA = oh.embeddings.get_klp_BM25(flat_klp)
 
 # %%
 """
-Label each klp pairwise combination (klp1 x klp2) as one of:
-    -1: klp1 is earlier than klp2
-    0: klp1 is equal to klp2
-    1: klp1 is later than klp2
+Label each klp pairwise combination (klp1 x klp2) for if klp1 is earlier or not
 """
 nklp = len(flat_klp)
-later_earlier = np.zeros((nklp, nklp), np.int8)
 
-for (i, klp1), (j, klp2) in tqdm(
-    product(enumerate(flat_klp.values()), repeat=2), total=nklp**2
-):
-    if klp1["yearOrder"] < klp2["yearOrder"]:
-        later_earlier[i, j] = -1
-    elif klp1["yearOrder"] == klp2["yearOrder"]:
-        if klp1["unitStudyOrder"] < klp2["unitStudyOrder"]:
-            later_earlier[i, j] = -1
-        elif klp1["unitStudyOrder"] > klp2["unitStudyOrder"]:
-            later_earlier[i, j] = 1
-    else:
-        later_earlier[i, j] = 1
+year_order = np.array([klp["yearOrder"] for klp in flat_klp.values()])
+unit_study_order = np.array([klp["unitStudyOrder"] for klp in flat_klp.values()])
+
+earlier = (year_order[:, None] < year_order[None]) | (
+    (year_order[:, None] == year_order[None])
+    & (unit_study_order[:, None] < unit_study_order[None])
+)
 
 
 # %%
 spA = sp.sparse.csr_array(klpA)
+
 # %%
 G = nx.from_numpy_array(klpA)
+
 # %%
 G = nx.from_scipy_sparse_array(spA)
 
 # %%
 # get lesson info from a klp
-key = 'biology-secondary-ks4-foundation-aqa/eukaryotic-and-prokaryotic-cells/cells/0'
-*lesson_key, klp_idx = key.split('/')
+key = "biology-secondary-ks4-foundation-aqa/eukaryotic-and-prokaryotic-cells/cells/0"
+*lesson_key, klp_idx = key.split("/")
 lesson_key = "/".join(lesson_key)
 lesson = lessons[l_df[l_df.lessonKey == lesson_key].index]
+
 # %%
