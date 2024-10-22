8243c1f151b85efa96b48428a73b635e7f090093
Robin Ince <robince@gmail.com>
2024-10-22 16:29:33 +0100

    indexing into klps based on relationship to target question

--- scripts/ri-embedding-similarity.py
+++ scripts/ri-embedding-similarity.py
@@ -4,7 +4,7 @@ from oakhack.utils import load_embeddings
 import numpy as np
 import pandas as pd
 
-#%%
+# %%
 programmes, units = oh.utils.load_oak_programmes_units()
 lessons, l_df = oh.utils.load_oak_lessons_with_df()
 questions, q_df = oh.utils.extract_questions(lessons)
@@ -16,29 +16,68 @@ flat_klp_l, klp_df = oh.utils.extract_klp_with_df(flat_klp)
 klp_embs = load_embeddings("klp_embeddings_batch_size3000_*.npy")
 q_embs = load_embeddings("question_embeddings_batch_size3000_*.npy")
 
-# %%
 # questions to klps within programmes
 cos_q_klp = {}
 for p in list(programmes):
     klp_idx = klp_df.loc[klp_df.programme == p].index.to_numpy()
     q_idx = q_df.loc[q_df.programmeSlug == p].index.to_numpy()
-    cos_q_klp[p] = np.dot(q_embs[q_idx,:], klp_embs[klp_idx,:].T)
+    cos_q_klp[p] = np.dot(q_embs[q_idx, :], klp_embs[klp_idx, :].T)
+
+# %%
+with open(oh.DATA_DIR / ".npz", "rb") as f:
+    dat = dict(np.load(f))
 
 
+# %%
 
 
-#%%
-cos_q_klp = np.dot(q_embs,klp_embs.T)
+# different unit
+p = "biology-secondary-ks4-foundation-aqa"
+programme_questions = q_df.loc[q_df.programmeSlug == p]
+q_select = 0  # index in programme questions
+q = questions[programme_questions.iloc[q_select].name]
+q_lessonkey = f"{q['programmeSlug']}/{q['unitSlug']}/{q['lessonSlug']}"
+q_unit = q["unitSlug"]
+
+# same lesson
+klp_samelesson_idx = klp_df.loc[
+    (klp_df.programme == p) & (klp_df.lesson_key == q_lessonkey)
+].index.to_numpy()
+
+# same-unit but different lesson
+klp_sameunit_idx = klp_df.loc[
+    (klp_df.programme == p) # within program
+    & (klp_df.lesson_key != q_lessonkey) # not same lesseon
+    & (klp_df.unit == q_unit) # same unit
+].index.to_numpy()
+
+# different unit
+klp_diffint_idx = klp_df.loc[
+    (klp_df.programme == p) & (klp_df.unit != q_unit)
+].index.to_numpy()
+
+
+# %%
+q_idx = q_df.loc[q_df.programmeSlug == p].index.to_numpy()
+q_same_lesson = q_df.loc[q_df.programmeSlug == p].index.to_numpy()
+cos = cos_q_klp[p]
+
+
+# %%
+cos_q_klp = np.dot(q_embs, klp_embs.T)
 
 # %%
 q_idx = 6
-q_klp_cos = np.dot(q_embs[q_idx,:], klp_embs.T)
+q_klp_cos = np.dot(q_embs[q_idx, :], klp_embs.T)
 # %%
-idx = klp_df.loc[klp_df.programme == 'biology-secondary-ks4-foundation-aqa'].index.to_numpy()
-top_klp = [flat_klp_l[i]['keyLearningPoint'] for i in np.argsort(q_klp_cos[idx])[-100:-1]][::-1]
+idx = klp_df.loc[
+    klp_df.programme == "biology-secondary-ks4-foundation-aqa"
+].index.to_numpy()
+top_klp = [
+    flat_klp_l[i]["keyLearningPoint"] for i in np.argsort(q_klp_cos[idx])[-100:-1]
+][::-1]
 
 # %%
 # top_klp = [flat_klp_l[i]['lesson'] for i in np.argsort(q_klp_cos[idx])[-100:-1]][::-1]
 top_klp = [klp_df.lesson.iloc[i] for i in np.argsort(q_klp_cos[idx])[-100:-1]][::-1]
 # %%
-
