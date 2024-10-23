fc68546c43782c297c2cc75ccca398c1b490176f
Robin Ince <robince@gmail.com>
2024-10-23 10:05:54 +0100

    plots of question similarity

--- scripts/ri-embedding-similarity.py
+++ scripts/ri-embedding-similarity.py
@@ -3,6 +3,9 @@ import oakhack as oh
 from oakhack.utils import load_embeddings
 import numpy as np
 import pandas as pd
+from collections import defaultdict
+from tqdm import tqdm
+import seaborn as sns
 
 # %%
 programmes, units = oh.utils.load_oak_programmes_units()
@@ -30,38 +33,62 @@ with open(oh.DATA_DIR / "similarity/bm25-q-klp-by-programme.npz", "rb")  as f:
     bm25_q_klp = d['arr_0'].item()
 
 # %%
+beta = 0.75 # embeddign weighting
+mix_q_klp = {}
+for p in cos_q_klp.keys():
+    mix_q_klp[p] = bm25_q_klp[p] + beta*cos_q_klp[p]
+
+# %%
 # based on a targe question, select KLPs related to the question in different ways
-p = "biology-secondary-ks4-foundation-aqa"
+p = "biology-secondary-ks4-higher-ocr"
 programme_questions = q_df.loc[q_df.programmeSlug == p]
 q_select = 0  # index in programme questions
 
-for q in range(len(programme_questions)):
+data_df = defaultdict(list)
+for q_select in tqdm(range(len(programme_questions))):
     q = questions[programme_questions.iloc[q_select].name]
     q_lessonkey = f"{q['programmeSlug']}/{q['unitSlug']}/{q['lessonSlug']}"
     q_unit = q["unitSlug"]
 
+    p_klp_df = klp_df.loc[(klp_df.programme == p)].copy().reset_index()
     # same lesson
-    klp_samelesson_idx = klp_df.loc[
-        (klp_df.programme == p) # same programmes
-        & (klp_df.lesson_key == q_lessonkey) # same lesson
+    klp_samelesson_idx = p_klp_df.loc[
+        (p_klp_df.lesson_key == q_lessonkey) # same lesson
     ].index.to_numpy()
 
     # same-unit but different lesson
-    klp_sameunit_idx = klp_df.loc[
-        (klp_df.programme == p) # within program
-        & (klp_df.lesson_key != q_lessonkey) # not same lesseon
-        & (klp_df.unit == q_unit) # same unit
+    klp_sameunit_idx = p_klp_df.loc[
+        (p_klp_df.lesson_key != q_lessonkey) # not same lesseon
+        & (p_klp_df.unit == q_unit) # same unit
     ].index.to_numpy()
 
     # different unit
-    klp_diffint_idx = klp_df.loc[
-        (klp_df.programme == p) # same programme
-        & (klp_df.unit != q_unit) # different unit
+    klp_diffint_idx = p_klp_df.loc[
+        (p_klp_df.unit != q_unit) # different unit
     ].index.to_numpy()
 
-    # mean cos 
-    # mean bm
-    # mean bm + 0.8 * cos 
+    sims = [bm25_q_klp[p], cos_q_klp[p], mix_q_klp[p]]
+    data_df["questionId"].extend(9*[q["questionId"]])
+        
+    data_df["avg_sim"].extend([np.mean(x[:,klp_samelesson_idx]) for x in sims])
+    data_df["measure"].extend(["bm25", "emb", "weighted"])
+    data_df["klps"].extend(3*["same-lesson"])
+
+    data_df["avg_sim"].extend([np.mean(x[:,klp_sameunit_idx]) for x in sims])
+    data_df["measure"].extend(["bm25", "emb", "weighted"])
+    data_df["klps"].extend(3*["same-unit"])
+
+    data_df["avg_sim"].extend([np.mean(x[:,klp_diffint_idx]) for x in sims])
+    data_df["measure"].extend(["bm25", "emb", "weighted"])
+    data_df["klps"].extend(3*["different-unit"])
+
+
+q_klp_mean_df = pd.DataFrame(data_df)
+
+# %%
+# visualise
+
+
 
 # %%
 q_idx = q_df.loc[q_df.programmeSlug == p].index.to_numpy()
